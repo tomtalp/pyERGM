27beb44485a0482d8021996922b169a7584c939b
Tom Talpir <tomtalp93@gmail.com>
2024-07-18 16:07:46 +0300

    Tests for NetworkStatistics object

---
+++ tests/test_utils.py
@@ -0,0 +1,41 @@
+import unittest
+from utils import *
+
+class TestNetworkStatistics(unittest.TestCase):
+    # def setUp(self):
+    #     pass      
+        
+    def test_initialization_params_validation(self):
+        with self.assertRaises(ValueError):
+            net_stats = NetworkStatistics()
+
+        with self.assertRaises(ValueError):
+            net_stats = NetworkStatistics(metric_names=["fake"])
+        
+        with self.assertRaises(ValueError):
+            net_stats = NetworkStatistics(metric_names=["num_edges"], custom_metrics={"fake": 1})
+
+    def test_calculate_statistics(self):
+        net_stats = NetworkStatistics(metric_names=["num_edges", "num_triangles"])
+        W = np.array([[0, 1, 1], 
+                      [1, 0, 1], 
+                      [1, 1, 0]])
+        stats = net_stats.calculate_statistics(W)
+        self.assertEqual(stats[0], 3)
+        self.assertEqual(stats[1], 3)
+
+        # Assert that num_triangles can't be run on a directed graph
+        with self.assertRaises(ValueError): 
+            net_stats.calculate_statistics(W, directed=True)
+
+        net_stats = NetworkStatistics(metric_names=["num_edges"])
+        W = np.array([[0, 1, 1], 
+                      [1, 0, 0], 
+                      [1, 0, 1]])
+        
+        stats = net_stats.calculate_statistics(W, directed=True)
+        self.assertEqual(stats[0], 5)
+        
+
+        
+
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import networkx as nx
 
-def connectivity_matrix_to_G(W: np.ndarray):
+def connectivity_matrix_to_G(W: np.ndarray, directed=False):
     """
     Convert a connectivity matrix to a graph object.
     
@@ -15,9 +15,11 @@ def connectivity_matrix_to_G(W: np.ndarray):
     G : nx.Graph
         A graph object.
 
-    ## TODO - 1. How do handle directed graphs?
     """
-    G = nx.from_numpy_array(W)
+    if directed:
+        G = nx.from_numpy_array(W, create_using=nx.DiGraph)
+    else:
+        G = nx.from_numpy_array(W)
     return G
 
 def get_random_nondiagonal_matrix_entry(n: int):
@@ -44,57 +46,62 @@ def get_random_nondiagonal_matrix_entry(n: int):
     return (i, j)
 
 class NetworkStatistics():
-    def __init__(self, statistic_names=[], custom_statistics={}):
+    def __init__(self, metric_names=[], custom_metrics={}):
         """
-        Initialize a NetworkStatistics object with a set of statistics.
+        Initialize a NetworkStatistics object with a set of metrics to be calculated.
         
         Parameters
         ----------
-        statistic_names : list
-            A list of names of statistics to calculate. 
-            Currently supported statistics are - 
+        metric_names : list
+            A list of names of metrics to calculate. 
+            Currently supported metrics are - 
                 [num_edges, num_triangles]
         
-        custom_statistics : dict
-            A custom statistic is registered via a key, value - 
+        custom_metrics : dict
+            A custom metric is registered via a key, value - 
                 key : str
-                    The name of the statistic.
+                    The name of the metric.
                 value : function
-                    A function that takes a graph object and returns the value of the statistic
+                    A function that takes a graph object and returns the value of the metric (i.e. the statistic value).
                 
             For example - 
                 {"sqrt_of_edges": lambda G: G.number_of_edges() ** 0.5}
 
         """
-        self._SUPPORTED_STATS = ["num_edges", "num_triangles"]
+        self._SUPPORTED_METRICS = ["num_edges", "num_triangles"]
 
-        self.statistic_names = statistic_names
-        self._validate_predefined_statistics()
+        self.metric_names = metric_names
+        self._validate_predefined_metrics()
 
-        self.custom_statistics = custom_statistics
-        self._validate_custom_statistics()      
+        self.custom_metrics = custom_metrics
+        self._validate_custom_metrics()      
 
-        self._register_statistics()        
+        self._register_metrics()        
     
-    def _validate_predefined_statistics(self):
-        for stat_name in self.statistic_names:
-            if stat_name not in self._SUPPORTED_STATS:
-                raise ValueError(f"Statistic {stat_name} is not supported.")
+    def _validate_predefined_metrics(self):
+        if len(self.metric_names) == 0:
+            raise ValueError("At least one metric must be specified.")
+        for metric_name in self.metric_names:
+            if metric_name not in self._SUPPORTED_METRICS:
+                raise ValueError(f"Metric {metric_name} is not supported.")
 
-    def _validate_custom_statistics(self):
-        for stat_name, stat_func in self.custom_statistics:
+    def _validate_custom_metrics(self):
+        for metric_name, stat_func in self.custom_metrics:
             if not callable(stat_func):
-                raise ValueError(f"Custom statistic {stat_name} is not a function.")
+                raise ValueError(f"Custom Metric {metric_name} is not a function.")
     
-    def _register_statistics(self):
-        self.statistics_functions = []
-        for stat_name in self.statistic_names:
-            if stat_name == "num_edges":
-                self.statistics_functions.append(lambda G: len(G.edges()))
-            elif stat_name == "num_triangles":
-                self.statistics_functions.append(lambda G: sum(nx.triangles(G)))
+    def _register_metrics(self):
+        self.statistics_functions = {}
+
+        for metric_name in self.metric_names:
+            if metric_name == "num_edges":
+                func = lambda G: len(G.edges())
+            elif metric_name == "num_triangles":
+                func = lambda G: sum(nx.triangles(G))
+
+            self.statistics_functions[metric_name] = func
     
-    def calculate_statistics(self, W: np.ndarray):
+    def calculate_statistics(self, W: np.ndarray, directed=False, verbose=False):
         """
         Calculate the statistics of a graph. 
         This is equivalent to calculating what is known in the ERGM literature as g(y) for a given graph y.
@@ -106,13 +113,20 @@ class NetworkStatistics():
         
         Returns
         -------
-        stats : np.ndarray
-            An array of statistics.
+        stats : np.ndarray or dict 
+            An array of statistics, represented as a numpy array of values, or a dict if verbose=True
         """
-        G = connectivity_matrix_to_G(W)
+        G = connectivity_matrix_to_G(W, directed=directed)
         
-        stats = [f(G) for f in self.statistics_functions]
-        
-        return np.array(stats)
+        if G.is_directed():
+            if "num_triangles" in self.metric_names:
+                raise ValueError("The 'num_triangles' metric is not supported for directed graphs.")
+
+        stats = {name: func(G) for name, func in self.statistics_functions.items()}
+        if verbose:
+            return stats
+        else:
+            return np.array(list(stats.values()))
+       
 
                 
