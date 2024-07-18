Fixes for network calculation statistics

ergm.py
@@ -1,6 +1,9 @@
 import numpy as np
 import networkx as nx
-from utils import connectivity_matrix_to_G
+import sampling
+
+from utils import *
+
 
 class ERGM():
     """
@@ -10,30 +13,17 @@ class ERGM():
     
     ## TODO - Thetas are currently set to 1 while in dev. We need to fit them!
     """
-    def __init__(self, n_nodes):
+    def __init__(self, n_nodes, network_statistics: NetworkStatistics):
         self._n_nodes = n_nodes
-        self._theta = []
+        self._thetas = None
         self._feature_functions = []
         self._normalization_factor = None ## TODO - Partition function is first calculated in fit. What should we initialize it to be?
-    
-    def register_feature_function(self, feature_function, theta):
-        """
-        ## TODO - 
-            1. When do we call this? At init? 
-            2. When are thetas set? Do we register features without setting thetas first?
-            
-            But in general - I think model should be initialized with a list of functions, so that the user
-            never has to manually call `register_feature_function()`.
-        """
-        self._feature_functions.append(feature_function)
-        self._theta.append(theta)
+        self._network_statistics = network_statistics
 
-    def _calculate_weight(self, W: np.ndarray):
-        G = connectivity_matrix_to_G(W)
 
-        features = [f(G) for f in self._feature_functions]
-
-        weight = np.exp(np.dot(self._theta, features))
+    def calculate_weight(self, W: np.ndarray):
+        features = self._network_statistics.calculate_statistics(W)
+        weight = np.exp(np.dot(self._thetas, features))
 
         return weight
     
@@ -44,16 +34,25 @@ class ERGM():
         self._normalization_factor = 0
 
         for network in networks_for_sample:
-            weight = self._calculate_weight(network)
+            weight = self.calculate_weight(network)
             self._normalization_factor += weight
 
 
-    def fit(self):
+    def fit(self, precalculated_normalization_factor=None, precalculated_thetas=None):
         """
         TODO - This is just a mock implementation. 
         Currently just calculating the normalization factor. 
         """
-        self._calculate_normalization_factor()
+        if precalculated_normalization_factor is not None:
+            self._normalization_factor = precalculated_normalization_factor
+        else:
+            self.normalization_factor = 100
+        
+        if precalculated_thetas is not None:
+            self._thetas = precalculated_thetas
+        else:
+            self._thetas = np.random.uniform(-1, 1, self._n_nodes)
+
 
     def calculate_probability(self, W: np.ndarray):
         """
@@ -70,15 +69,15 @@ class ERGM():
             The probability of the graph under the ERGM model.
         """
 
-        if self._normalization_factor is None:
-            raise ValueError("Normalization factor not set, fit the model before calculating probability.")
+        if self._normalization_factor is None or self._thetas is None:
+            raise ValueError("Normalization factor and thetas not set, fit the model before calculating probability.")
 
-        weight = self._calculate_weight(W)
+        weight = self.calculate_weight(W)
         prob = weight / self._normalization_factor
         
         return prob
 
-    def sample_network(self, seed_network=None, steps=500, burn_in=100):
+    def sample_network(self, sampling_type="gibbs", seed_network=None, steps=500):
         """
         Sample a network from the ERGM model.
         
@@ -97,22 +96,18 @@ class ERGM():
         W : np.ndarray
             The sampled connectivity matrix.
         """
-        if self._normalization_factor is None:
+        if self._normalization_factor is None: # Not sure 
             raise ValueError("Normalization factor not set, fit the model before sampling.")
 
-        if not seed_network:
-            W = np.random.randint(0, 2, (self._n_nodes, self._n_nodes))
+        if sampling_type == "gibbs":
+            sampler = sampling.Gibbs(self._network_statistics)
+        else:
+            raise ValueError(f"Sampling type {sampling_type} not supported.")
         
-        
+        if seed_network is None:
+            G = nx.erdos_renyi_graph(self._n_nodes, 0.4)
+            seed_network = nx.to_numpy_array(G)
 
+        network = sampler.sample(seed_network, n_iter=steps)
 
-
-            # # Propose a new graph
-            # W_proposed = self.propose(W)
-            # # Calculate the acceptance probability
-            # acceptance_prob = self.calculate_acceptance_prob(W_proposed)
-            # # Accept or reject the proposal
-            # if acceptance_prob > np.random.rand():
-            #     W = W_proposed
-        
-
+        return network

sampling.py
@@ -9,19 +9,16 @@ class Sampler():
         pass
 
 class Gibbs(Sampler):
-    def __init__(self, feature_functions):
+    def __init__(self, network_stats_calculator):
         super().__init__()
 
-        self.feature_functions = feature_functions
+        self.network_stats_calculator = network_stats_calculator
     
-    def _calculate_diff_statistics(self, W1, W2):
-        G1 = connectivity_matrix_to_G(W1)
-        G2 = connectivity_matrix_to_G(W2)
-
-        stats1 = [f(G1) for f in self.feature_functions]
-        stats2 = [f(G2) for f in self.feature_functions]
-
-        diff_stats = np.array(stats2) - np.array(stats1)
+    def _calculate_diff_statistics(self, y_plus, y_minus):
+        stats1 = self.network_stats_calculator.calculate_statistics(y_plus)
+        stats2 = self.network_stats_calculator.calculate_statistics(y_minus)
+        print(f"stats1: {stats1}, stats2: {stats2}")
+        diff_stats = stats1 - stats2
 
         return diff_stats
 
@@ -31,11 +28,24 @@ class Gibbs(Sampler):
         network = seed_network.copy()
 
         for i in range(n_iter):
+            print(f"iter {i}")
             random_entry = get_random_nondiagonal_matrix_entry(n)
+            
+            network[random_entry] = 1
+            network[random_entry[::-1]] = 1
 
             perturbed_net = network.copy()
-            perturbed_net[random_entry] = 1 - perturbed_net[random_entry]
+            perturbed_net[random_entry] = 0
+            perturbed_net[random_entry[::-1]] = 0
 
+            print("network")
+            print(network)
+            print("perturbed_net")
+            print(perturbed_net)
+
+            diff_stats = self._calculate_diff_statistics(network, perturbed_net)
+            print("DIFF STATS - ")
+            print(diff_stats)
             
 
 
@@ -43,17 +53,17 @@ class Gibbs(Sampler):
         
 
 
-# class MCMC():
-#     def __init__(self, G, model):
-#         self.G = G
-#         self.model = model
+# # class MCMC():
+# #     def __init__(self, G, model):
+# #         self.G = G
+# #         self.model = model
 
-#     def sample(self, n_iter):
-#         for i in range(n_iter):
-#             # Propose a new graph
-#             G_proposed = self.propose()
-#             # Calculate the acceptance probability
-#             acceptance_prob = self.calculate_acceptance_prob(G_proposed)
-#             # Accept or reject the proposal
-#             if acceptance_prob > np.random.rand():
-#                 self.G = G_proposed
+# #     def sample(self, n_iter):
+# #         for i in range(n_iter):
+# #             # Propose a new graph
+# #             G_proposed = self.propose()
+# #             # Calculate the acceptance probability
+# #             acceptance_prob = self.calculate_acceptance_prob(G_proposed)
+# #             # Accept or reject the proposal
+# #             if acceptance_prob > np.random.rand():
+# #                 self.G = G_proposed

tests/test_utils.py
@@ -22,7 +22,7 @@ class TestNetworkStatistics(unittest.TestCase):
                       [1, 1, 0]])
         stats = net_stats.calculate_statistics(W)
         self.assertEqual(stats[0], 3)
-        self.assertEqual(stats[1], 3)
+        self.assertEqual(stats[1], 1)
 
         # Assert that num_triangles can't be run on a directed graph
         with self.assertRaises(ValueError): 
@@ -35,6 +35,22 @@ class TestNetworkStatistics(unittest.TestCase):
         
         stats = net_stats.calculate_statistics(W, directed=True)
         self.assertEqual(stats[0], 5)
+
+        net_stats = NetworkStatistics(metric_names=["num_edges", "num_triangles"])
+        W = np.array([[0, 0, 0], 
+                      [0, 0, 0], 
+                      [0, 0, 0]])
+        stats = net_stats.calculate_statistics(W)
+        self.assertEqual(stats[0], 0)
+        self.assertEqual(stats[1], 0)
+    
+    def test_get_num_of_statistics(self):
+        net_stats = NetworkStatistics(metric_names=["num_edges"])
+        self.assertEqual(net_stats.get_num_of_statistics(), 1)
+
+        net_stats = NetworkStatistics(metric_names=["num_edges", "num_triangles"])
+        self.assertEqual(net_stats.get_num_of_statistics(), 2)
+
         
 
         

utils.py
@@ -97,9 +97,20 @@ class NetworkStatistics():
             if metric_name == "num_edges":
                 func = lambda G: len(G.edges())
             elif metric_name == "num_triangles":
-                func = lambda G: sum(nx.triangles(G))
+                func = lambda G: sum(nx.triangles(G).values()) / len(G.nodes())
 
             self.statistics_functions[metric_name] = func
+    
+    def get_num_of_statistics(self):
+        """
+        Get the number of statistics that are registered.
+        
+        Returns
+        -------
+        n_stats : int
+            The number of statistics.
+        """
+        return len(self.statistics_functions)
     
     def calculate_statistics(self, W: np.ndarray, directed=False, verbose=False):
         """