aa85ca0c21839dd3fb845eea2a82bfab1c187c0e
Oren Richter <orenri@wismain.weizmann.ac.il>
2024-08-06 18:17:42 +0300

    implemented the algorithm to find the greatest convex minorant of n points

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,17 +1,20 @@
 import unittest
 from utils import *
 
+from matplotlib import pyplot as plt
+
+
 # class TestNetworkStatistics(unittest.TestCase):
 #     # def setUp(self):
 #     #     pass      
-        
+
 #     def test_initialization_params_validation(self):
 #         with self.assertRaises(ValueError):
 #             net_stats = NetworkStatistics()
 
 #         with self.assertRaises(ValueError):
 #             net_stats = NetworkStatistics(metric_names=["fake"])
-        
+
 #         with self.assertRaises(ValueError):
 #             net_stats = NetworkStatistics(metric_names=["num_edges"], custom_metrics={"fake": 1})
 
@@ -32,7 +35,7 @@ from utils import *
 #         W = np.array([[0, 1, 1], 
 #                       [1, 0, 0], 
 #                       [1, 0, 1]])
-        
+
 #         stats = net_stats.calculate_statistics(W)
 #         self.assertEqual(stats[0], 5)
 
@@ -43,4 +46,78 @@ from utils import *
 #         stats = net_stats.calculate_statistics(W)
 #         self.assertEqual(stats[0], 0)
 #         self.assertEqual(stats[1], 0)
-    
+
+
+class TestGreatestConvexMinorant(unittest.TestCase):
+    DO_PLOT = False
+
+    def test_not_change_values_of_convex_functions(self):
+        xs = np.arange(10) + 1
+        convex_func_vals = 1 / xs ** 2
+
+        minorant_vals = get_greatest_convex_minorant(xs, convex_func_vals)
+
+        if TestGreatestConvexMinorant.DO_PLOT:
+            plt.plot(xs, convex_func_vals, '.k', label='input values')
+            plt.plot(xs, minorant_vals, label='greatest convex minorant')
+            plt.legend()
+            plt.show()
+
+        self.assertTrue(np.all(minorant_vals == convex_func_vals))
+
+    def test_engineered_scenario(self):
+        def get_expected_value(convex_func_vals, wrapping_indices, perturbed_idx):
+            slope = (convex_func_vals[wrapping_indices[1]] - convex_func_vals[wrapping_indices[0]]) / (
+                    xs[wrapping_indices[1]] - xs[wrapping_indices[0]])
+            return slope * xs[perturbed_idx] + convex_func_vals[wrapping_indices[1]] - xs[wrapping_indices[1]] * slope
+
+        num_points = 10
+        xs = np.arange(num_points) + 1
+        convex_func_vals = np.exp(-xs)
+        num_scenarios = 4
+        perturbation = 0.5
+        indices_to_increase = np.array([[1, 3, 7], [1, 2, 3], [0, 2, 4], [5, 6, 9]]).astype(int)
+        expected_values = np.array([convex_func_vals, ] * num_scenarios)
+        expected_values[0, 1] = get_expected_value(convex_func_vals, (0, 2), 1)
+        expected_values[0, 3] = get_expected_value(convex_func_vals, (2, 4), 3)
+        expected_values[0, 7] = get_expected_value(convex_func_vals, (6, 8), 7)
+
+        expected_values[1, 1] = get_expected_value(convex_func_vals, (0, 4), 1)
+        expected_values[1, 2] = get_expected_value(convex_func_vals, (0, 4), 2)
+        expected_values[1, 3] = get_expected_value(convex_func_vals, (0, 4), 3)
+
+        expected_values[2, 0] = convex_func_vals[0] + perturbation
+        expected_values[2, 2] = get_expected_value(convex_func_vals, (1, 3), 2)
+        expected_values[2, 4] = get_expected_value(convex_func_vals, (3, 5), 4)
+
+        expected_values[3, 5] = get_expected_value(convex_func_vals, (4, 7), 5)
+        expected_values[3, 6] = get_expected_value(convex_func_vals, (4, 7), 6)
+        expected_values[3, 9] = convex_func_vals[9] + perturbation
+
+        for i in range(num_scenarios):
+            cur_vals = convex_func_vals.copy()
+            cur_vals[indices_to_increase[i]] += perturbation
+            minorant_vals = get_greatest_convex_minorant(xs, cur_vals)
+            if TestGreatestConvexMinorant.DO_PLOT:
+                plt.plot(xs, cur_vals, '.k', label='input values')
+                plt.plot(xs, minorant_vals, label='greatest convex minorant')
+                plt.legend()
+                plt.title(f'scenario {i}')
+                plt.show()
+
+            for j in range(num_points):
+                self.assertAlmostEqual(minorant_vals[j], expected_values[i, j], places=8)
+
+    def test_random_points(self):
+        np.random.seed(8972634)
+        num_points = 100
+        xs = np.arange(num_points) + 1
+        values = 10 * np.random.rand(100)
+        minorant_vals = get_greatest_convex_minorant(xs, values)
+        if TestGreatestConvexMinorant.DO_PLOT:
+            plt.plot(xs, values, '.k', label='input values')
+            plt.plot(xs, minorant_vals, label='greatest convex minorant')
+            plt.legend()
+            plt.show()
+        self.assertTrue(np.all(minorant_vals <= values))
+        self.assertTrue(np.all(np.diff(minorant_vals, n=2) >= -10 ** -10))
--- utils.py
+++ utils.py
@@ -50,6 +50,7 @@ def get_random_nondiagonal_matrix_entry(n: int):
 
     return tuple(np.random.choice(n, size=2, replace=False))
 
+
 def construct_adj_mat_from_int(int_code: int, num_nodes: int, is_directed: bool) -> np.ndarray:
     num_pos_connects = num_nodes * (num_nodes - 1)
     if not is_directed:
@@ -77,3 +78,57 @@ def construct_int_from_adj_mat(adj_mat: np.ndarray, is_directed: bool) -> int:
         adj_mat_no_diag = adj_mat_no_diag[:adj_mat_no_diag.size // 2]
     return round((adj_mat_no_diag * 2 ** np.arange(adj_mat_no_diag.size - 1, -1, -1).astype(np.ulonglong)).sum())
 
+
+def _calc_line_slope_2_points(xs: np.ndarray, ys: np.ndarray):
+    if xs.size != 2 or ys.size != 2:
+        raise ValueError(
+            f"The sizes of xs and ys must be 2 to form a well defined line! Got {xs.size}, {ys.size} instead")
+    return (xs)
+
+
+def get_greatest_convex_minorant(xs: np.ndarray, ys: np.ndarray):
+    if xs.size != ys.size:
+        raise ValueError("Arrays must have the same size!")
+    # For numerical considerations we don't force the sequence of slopes to really not decrease
+    slope_diff_thr = -10 ** -10
+    x_diffs = np.diff(xs)
+    # TODO: this case be handled (reduce the larger y value to the lower one, perform the same logic for the effective
+    #  n-1 points, and duplicate the relevant point at the end). But is seems a very specific case and I'm not sure it's
+    #  worth the complication.
+    if np.any(x_diffs == 0):
+        raise ValueError("xs array must contain unique values!")
+    cur_proposed_minorant = ys.copy()
+    cur_spline_slopes = np.diff(cur_proposed_minorant) / x_diffs
+    cur_decreasing_slope_indices = np.where(np.diff(cur_spline_slopes) < slope_diff_thr)[0]
+    # Convexity condition - the slopes are monotonically increasing
+    while cur_decreasing_slope_indices.size > 0:
+        # The indices of the points that violate the condition, and whose y values need to be decreases are in 1 shift
+        # from the problematic slopes (if the second slope is smaller than the first slope, cur_decreasing_slope_indices
+        # would include the index 0, of the first slopes difference. This corresponds to the point that takes part in
+        # the calculations of both slopes, the first and the second, which is the second point, with index 1).
+        cur_problematic_indices = cur_decreasing_slope_indices + 1
+
+        # These are the indices that wrap the problematic ones (i.e., all the indices that are between wrapping_indices
+        # are problematic).
+        # Note: problematic indices may be subsequent (indices 2 and 3 are problematic, so we want 1, 4 to wrap them).
+        cur_wrapping_indices = np.array([i for i in range(xs.size) if (i not in cur_problematic_indices and (
+                i + 1 in cur_problematic_indices or i - 1 in cur_problematic_indices))]).astype(int)
+
+        # To get an equation of a line from the slope and a point on in we can rearrange the formula:
+        # y-y0 = m(x-x0) -> y = mx + (y0-m*x0)
+        cur_wrapping_lines_slopes = np.diff(cur_proposed_minorant[cur_wrapping_indices]) / np.diff(
+            xs[cur_wrapping_indices])
+        cur_wrapping_lines_intersepts = (cur_proposed_minorant[cur_wrapping_indices[:-1]] -
+                                         cur_wrapping_lines_slopes * xs[cur_wrapping_indices[:-1]])
+
+        # Update the values in problematic indices to lie on the corresponding wrapping line
+        cur_assignment_problematic_idx_to_line = np.searchsorted(cur_wrapping_indices, cur_problematic_indices) - 1
+        cur_proposed_minorant[cur_problematic_indices] = (
+                cur_wrapping_lines_slopes[cur_assignment_problematic_idx_to_line] * xs[cur_problematic_indices] +
+                cur_wrapping_lines_intersepts[cur_assignment_problematic_idx_to_line])
+
+        # Update the slopes and the indices where they decrease
+        cur_spline_slopes = np.diff(cur_proposed_minorant) / x_diffs
+        cur_decreasing_slope_indices = np.where(np.diff(cur_spline_slopes) < slope_diff_thr)[0]
+
+    return cur_proposed_minorant
