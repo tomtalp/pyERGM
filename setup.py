6316b5e6a183399c63237183bb373bff460c0818
Tom Talpir <tomtalp93@gmail.com>
2024-10-28 18:03:27 +0200

    setup.py version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyERGM",
-    version="0.1.0",
+    version="0.1.1",
     description="Exponential Random Graph Models in Python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
