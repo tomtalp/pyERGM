fixed some dim issues due to the transition to scipy.optimize.minimize and played with optimization methods, haven't solved the scaling issue of MPLE regressors yet

pyERGM/ergm.py
@@ -160,11 +160,16 @@ class ERGM():
             The estimated coefficients of the ERGM.
         """
         print(f"MPLE with lr {lr}")
-        trained_thetas, prediction = mple_logistic_regression_optimization(self._metrics_collection, observed_network,
-                                                                           is_distributed=self._is_distributed_optimization,
-                                                                           lr=lr,
-                                                                           stopping_thr=stopping_thr,
-                                                                           max_iter=logistic_reg_max_iter)
+        # trained_thetas, prediction = mple_logistic_regression_optimization(self._metrics_collection, observed_network,
+        #                                                                    is_distributed=self._is_distributed_optimization,
+        #                                                                    lr=lr,
+        #                                                                    stopping_thr=stopping_thr,
+        #                                                                    max_iter=logistic_reg_max_iter)
+
+        trained_thetas, prediction = scipy_mple_logistic_regression_optimization(self._metrics_collection,
+                                                                                 observed_network,
+                                                                                 is_distributed=self._is_distributed_optimization)
+
         self._exact_average_mat = np.zeros((self._n_nodes, self._n_nodes))
 
         if self._is_directed:
@@ -477,16 +482,19 @@ class ERGM():
                 num_model_sub_samples = kwargs.get("num_model_sub_samples", 100)
                 model_subsample_size = kwargs.get("model_subsample_size", 1000)
                 mahalanobis_dists = np.zeros(num_model_sub_samples)
-                
-                sub_sample_indices = np.random.choice(np.arange(mcmc_sample_size), size=num_model_sub_samples * model_subsample_size)
+
+                sub_sample_indices = np.random.choice(np.arange(mcmc_sample_size),
+                                                      size=num_model_sub_samples * model_subsample_size)
 
                 sub_samples = networks_for_sample[:, :, sub_sample_indices]
                 sub_samples_features = self._metrics_collection.calculate_sample_statistics(sub_samples)
-                mean_per_subsample = sub_samples_features.reshape(num_of_features, num_model_sub_samples, model_subsample_size).mean(axis=2)
+                mean_per_subsample = sub_samples_features.reshape(num_of_features, num_model_sub_samples,
+                                                                  model_subsample_size).mean(axis=2)
 
                 for cur_subsam_idx in range(num_model_sub_samples):
                     sub_sample_mean = mean_per_subsample[:, cur_subsam_idx]
-                    mahalanobis_dists[cur_subsam_idx] = mahalanobis(observed_features, sub_sample_mean, inv_observed_covariance)
+                    mahalanobis_dists[cur_subsam_idx] = mahalanobis(observed_features, sub_sample_mean,
+                                                                    inv_observed_covariance)
 
                 bootstrap_convergence_confidence = kwargs.get("bootstrap_convergence_confidence", 0.95)
                 bootstrap_convergence_num_stds_away_thr = kwargs.get("bootstrap_convergence_num_stds_away_thr", 1)

pyERGM/utils.py
@@ -636,38 +636,41 @@ def local_mple_logistic_regression_optimization_step(Xs, ys, thetas):
     return prediction, log_like, grad, hessian
 
 
-def minus_log_likelihood_local(thetas, Xs, ys):
-    pred = calc_logistic_regression_predictions(Xs, thetas)
+def minus_log_likelihood_local(thetas, Xs, ys, eps=1e-10):
+    pred = np.clip(calc_logistic_regression_predictions(Xs, thetas.reshape(thetas.size, 1)), eps, 1-eps)
     return -calc_logistic_regression_predictions_log_likelihood(pred, ys)
 
 
 def minus_log_likelihood_distributed(thetas, data_path, num_edges_per_job):
-    return -distributed_logistic_regression_optimization_step(data_path, thetas, 'log_likelihood', num_edges_per_job)
+    return -distributed_logistic_regression_optimization_step(data_path, thetas.reshape(thetas.size, 1),
+                                                              'log_likelihood', num_edges_per_job)
 
 
-def minus_log_like_grad_local(thetas, Xs, ys):
-    pred = calc_logistic_regression_predictions(Xs, thetas)
-    return -calc_logistic_regression_log_likelihood_grad(Xs, pred, ys)
+def minus_log_like_grad_local(thetas, Xs, ys, eps=1e-10):
+    pred = np.clip(calc_logistic_regression_predictions(Xs, thetas.reshape(thetas.size, 1)), eps, 1-eps)
+    return -calc_logistic_regression_log_likelihood_grad(Xs, pred, ys).reshape(thetas.size,)
 
 
 def minus_log_like_grad_distributed(thetas, data_path, num_edges_per_job):
-    return -distributed_logistic_regression_optimization_step(data_path, thetas, 'log_likelihood_gradient',
+    return -distributed_logistic_regression_optimization_step(data_path, thetas.reshape(thetas.size, 1),
+                                                              'log_likelihood_gradient',
                                                               num_edges_per_job)
 
 
-def minus_log_likelihood_hessian_local(thetas, Xs, ys):
-    pred = calc_logistic_regression_predictions(Xs, thetas)
+def minus_log_likelihood_hessian_local(thetas, Xs, ys, eps=1e-10):
+    pred = np.clip(calc_logistic_regression_predictions(Xs, thetas.reshape(thetas.size, 1)), eps, 1-eps)
     return -calc_logistic_regression_log_likelihood_hessian(Xs, pred)
 
 
 def minus_log_likelihood_hessian_distributed(thetas, data_path, num_edges_per_job):
-    return -distributed_logistic_regression_optimization_step(data_path, thetas, 'log_likelihood_hessian',
+    return -distributed_logistic_regression_optimization_step(data_path, thetas.reshape(thetas.size, 1),
+                                                              'log_likelihood_hessian',
                                                               num_edges_per_job)
 
 
-def scipy_mple_logistic_regression_optimization_step(metrics_collection, observed_network: np.ndarray,
-                                                     initial_thetas: np.ndarray | None = None,
-                                                     is_distributed: bool = False):
+def scipy_mple_logistic_regression_optimization(metrics_collection, observed_network: np.ndarray,
+                                                initial_thetas: np.ndarray | None = None,
+                                                is_distributed: bool = False):
     def after_iteration_callback(intermediate_result: OptimizeResult):
         nonlocal iteration
         iteration += 1
@@ -700,20 +703,22 @@ def scipy_mple_logistic_regression_optimization_step(metrics_collection, observe
 
     num_features = metrics_collection.calc_num_of_features()
     if initial_thetas is None:
-        thetas = np.random.rand(num_features, 1)
+        thetas = np.random.rand(num_features)
     else:
         thetas = initial_thetas.copy()
 
     if not is_distributed:
         res = minimize(minus_log_likelihood_local, thetas, args=(Xs, ys),
                        jac=minus_log_like_grad_local, hess=minus_log_likelihood_hessian_local,
-                       callback=after_iteration_callback)
+                       callback=after_iteration_callback, method="Newton-CG")
     else:
-        num_edges_per_job = 5000 # TODO: make this configurable
+        num_edges_per_job = 5000  # TODO: make this configurable
         res = minimize(minus_log_likelihood_distributed, thetas, args=(data_path, num_edges_per_job),
                        jac=minus_log_like_grad_distributed, hess=minus_log_likelihood_hessian_distributed,
-                       callback=after_iteration_callback)
-    return res
+                       callback=after_iteration_callback, method="Newton-CG")
+    print(res)
+    sys.stdout.flush()
+    return res.x, calc_logistic_regression_predictions(Xs, res.x.reshape(-1, 1)).flatten()
 
 
 def mple_logistic_regression_optimization(metrics_collection, observed_network: np.ndarray,